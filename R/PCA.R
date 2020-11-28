1a0fe05cd1998e8e8a9184bd24f15e8d4cabef34
Isaac Shaffer <ins2@nau.edu>
2020-11-27 23:04:48 -0700

    Initial commit

---
+++ R/PCA.R
@@ -0,0 +1,48 @@
+#' A Principal Components Analysis tool
+#' based on single value decomposition
+#'
+#'
+#' @param data - An N x P matrix of N observations on P features.
+#' @param scale - If TRUE features are scaled AND centered. If FALSE features are only centered. (default: FALSE)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+PCA <- function(data, scale = FALSE) {
+  # A simple column sd function
+  colSd <- function(data) {
+    data.mat <- as.matrix(data)
+    return(apply(data.mat, 2, sd) )
+  }
+  # Coerce data.frame or data.table to matrix
+  # Use separate step for better trace back
+  data.mat <- as.matrix(data)
+  if (scale) {
+    # Calculate scale vector
+    scale.out = colSd(data.mat)
+    # Calculate mean/center vector
+    center.out = colMeans((data.mat))
+    # Scale and center data
+    scaled_centered_data.mat <- t(apply(data.mat, 1,
+                                        function(x){(x-center.out)/scale.out}))
+  } else {
+    scale.out = FALSE
+    # Calculate mean/center vector
+    center.out = colMeans((data.mat))
+    # Center data
+    scaled_centered_data.mat <- t(apply(data.mat, 1, function(x){x-center.out}))
+  }
+  # Estimate spectral decomposition for centered (and scaled?)
+  # data
+  svd_results.list <- svd(scaled_centered_data.mat)
+  # Extract rotation matrix providied in prcomp results
+  rotation <- svd_results.list$v
+  # calculate projections from data onto all principal components
+  x <- scaled_centered_data.mat %*% rotation
+  # Return a list of results similar to prcomp
+  return(list(rotation = rotation,
+              center = center.out,
+              scale = scale.out,
+              x = x))
+}
---
+++ R/PCARE.R
@@ -0,0 +1,51 @@
+#' Calculates reconstruction error for Principal Components
+#' Analysis (PCA) with output from stats::prcomp() or PCA() in this package.
+#'
+#' @param data - original data used to create PCA model
+#' @param mod.pca - a PCA or stats::prcomp model
+#' @param max_components - Maximum number of PCs to include
+#'
+#' @return
+#' @export
+#'
+#' @examples
+PCAReconstructionError <- function(data, mod.pca, max_components) {
+  require(data.table)
+  if (missing(max_components)) {max_components <- ncol(mod.pca$rotation)}
+  data.mat <- as.matrix(data)
+
+  # Deal with possibly scaled data. Subtracting mean here and
+  # possibly scaling
+  if ((is.logical(mod.pca$scale)) && (!mod.pca$scale)) {
+    scaled_centered_data.mat <- scale(data.mat, center = TRUE, scale = FALSE)
+  } else {
+    scaled_centered_data.mat <- scale(data.mat, center = TRUE, scale = TRUE)
+  }
+  # Initialize results list for do.call/rbind recovery
+  # after loop
+  RE_data.list <- list()
+  # Set previous fit values. Would be the mean.mat if not
+  # for the centering above.
+  previous_fit.mat <- matrix(rep(0, nrow(data.mat)*ncol(data.mat)),
+                             nrow = nrow(data.mat))
+  # Loop over desired number of principal components for calculate
+  # reconstruction error
+  for (current_PC in 1:max_components){
+    # Build a matrix of partial projections for each
+    # projected data point given the current principal component
+    this_PC <- mod.pca$rotation[,current_PC]
+    PC.mat <- matrix(this_PC, nrow=nrow(data.mat), ncol=ncol(data.mat), byrow=TRUE)
+    # Calculate projection data projection onto onto all
+    # principal components up to the current component
+    fit.mat <- previous_fit.mat + PC.mat * mod.pca$x[, current_PC]
+    # Calculate the Reconstruction Error as described in
+    # Elements of Statistical Learning, Eq 14.50
+    RE_data.list[[paste(current_PC)]] <- data.table(
+      RE = sum((scaled_centered_data.mat - fit.mat)^2),
+      Components = current_PC)
+    # Save fit results for next iteration
+    previous_fit.mat <- fit.mat
+  }
+  # return results as a data.table
+  return(do.call(rbind, RE_data.list))
+}
